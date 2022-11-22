48b08ca5514dc203d41455077738a0945899dacb
shivansh patel <49273557+thisisshivanshpatel@users.noreply.github.com>
2022-11-22 04:40:54 +0000

    userSchema created in model and post route created in app.js

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 require('dotenv').config()
 require("./database/database").connect()
 const express=require('express');
+const { db } = require('./database/model/user');
 const app=express()
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send('<h1>Welcome to the CodeSpace Express server')
 })
+
+app.post("/register",async(req,res)=>{
+    try {
+        //get all data from body
+        //all the data should exists
+        //check if user already exists
+        //encrypt the password
+        // save the user in db
+        // generate a token for user and send it
+    } catch (error) {
+        console.error(error);
+    }
+})
+
 
 module.exports=app;
---
+++ database/model/user.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    firstname:{
+        type:String,
+        default:null
+    },
+    lastname:{
+        type:String,
+        default:null
+    }
+    ,
+    email:{
+        type:String,
+        unique:true
+    }
+    ,
+    password:{
+        type:String,
+        default:null
+    }
+    ,
+    token:{
+        type:String,
+        default:null
+    }
+})
+
+module.exports=mongoose.model("user",userSchema);
